f8401904b5d5d60c0df0e77c2f4bab9fb549f03e
NikoDood <162916390+NikoDood@users.noreply.github.com>
2024-09-06 10:39:45 +0200

    Initial EF core setup

---
+++ DTO/LoginRequest .cs
@@ -0,0 +1,9 @@
+﻿namespace WhoKnows_backend.DTO
+{
+    public class LoginRequest2
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using WhoKnows_backend.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure DbContext with Pomelo.EntityFrameworkCore.MySql
+builder.Services.AddDbContext<WhoknowsContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 32)) // Replace with your MySQL version
+    );
+});
 
 var app = builder.Build();
 
