1f155437192cd5f8b8f57f2889ca658aa8b98cad
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-09-28 22:40:38 +0200

    removed the HttpGet("http") endpoint

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -17,8 +17,7 @@ namespace WhoKnows_backend.Controllers
             _context = context;
         }
 
-        // HTML endpoint for search
-        [HttpGet("Html")]
+        [HttpGet("")]
         public async Task<IActionResult> Search([FromQuery] string q = null, [FromQuery] string language = "en")
         {
             var searchResults = string.IsNullOrEmpty(q)
@@ -30,18 +29,7 @@ namespace WhoKnows_backend.Controllers
             return Ok(searchResults); 
         }
 
-        // API endpoint for search
-        [HttpGet("")]
-        public async Task<IActionResult> ApiSearch([FromQuery] string q = null, [FromQuery] string language = "en")
-        {
-            var searchResults = string.IsNullOrEmpty(q)
-                ? new List<Page>()
-                : await _context.Pages
-                    .Where(p => p.Language == language && p.Content.Contains(q))
-                    .ToListAsync();
-
-            return Ok(new { searchResults });
-        }
+  
     }
 }
 
